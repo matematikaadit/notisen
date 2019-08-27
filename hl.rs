61a11ad09d7eb36db88f797e63450321181143ae
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2019-08-27 16:56:57 +0700

    License

--- hl.rs
+++ hl.rs
@@ -25,7 +25,7 @@ use plugin::{t_weechat_plugin, WEECHAT_RC_OK, WEECHAT_PLUGIN_API_VERSION};
 #[no_mangle] pub static weechat_plugin_author:      [u8; 47] = *b"Adit Cahya Ramadhan <matematikaadit@gmail.com>\0";
 #[no_mangle] pub static weechat_plugin_description: [u8; 48] = *b"Collect highlights/mentions in a single buffer.\0";
 #[no_mangle] pub static weechat_plugin_version:     [u8;  4] = *b"0.1\0";
-#[no_mangle] pub static weechat_plugin_license:     [u8;  4] = *b"MIT\0";
+#[no_mangle] pub static weechat_plugin_license:     [u8;  5] = *b"GPL3\0";
 #[no_mangle] pub static weechat_plugin_priority: u32 = 1000; // default priority
 
 #[no_mangle] pub static mut weechat_plugin: *mut t_weechat_plugin = null_mut();
@@ -33,13 +33,19 @@ use plugin::{t_weechat_plugin, WEECHAT_RC_OK, WEECHAT_PLUGIN_API_VERSION};
 #[no_mangle]
 pub extern "C" fn weechat_plugin_init(
     plugin: *mut t_weechat_plugin,
-    _argc: i32,
-    _argv: *const *const u8
+    argc: i32,
+    argv: *const *const u8
 ) -> i32 {
     unsafe {
         weechat_plugin = plugin;
         let printf_date_tags = (*weechat_plugin).printf_date_tags;
-        printf_date_tags(null_mut(), 0, null(), b"==> Hello, World! <==\0".as_ptr());
+        let print = |s| {
+            printf_date_tags(null_mut(), 0, null(), s);
+        };
+        print(format!("==> [hl] {} argv:\0", argc).as_ptr());
+        for i in 0..argc {
+            print(*argv.offset(i as isize));
+        }
         WEECHAT_RC_OK
     }
 }
